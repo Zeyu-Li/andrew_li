a481f863ad2f1ead4d8dc0e8402e310152c864c2
Andrew <zeyu.li23@gmail.com>
2019-03-10 21:48:26 -0600

    deleted old dir

--- Personal Brand Site/_js/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-$(document).ready(function(){ 
-    $('.backtotop').click(function(){ 
-        $("html, body").animate({ scrollTop: 0 }, 300); 
-        return false; 
-    }); 
-});
