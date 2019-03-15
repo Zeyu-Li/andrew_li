f0eb515f9d45efd50f1968fb5d818d04becd74b4
Zeyu-Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-03-15 12:04:28 -0600

    finished working copy of website

--- _js/index.js
+++ _js/index.js
@@ -5,3 +5,34 @@ $(document).ready(function(){
         return false; 
     }); 
 });
+var active = false;
+$(document).ready(function(){ 
+    
+    if (active == false) {
+        $('.banner, .barActive').mouseenter(function(){ 
+            $(".barActive").show(); 
+        }).mouseleave(function(){
+            if (active != true) {
+                $(".barActive").hide(); 
+            }
+        });
+    }
+
+    $('.banner').click(function(){
+        if ($('.barActive').css("visibility") != "hidden" && active == false) {
+            $(".barActive").show(); 
+            active = true;
+            
+        } else {
+            $(".barActive").hide(); 
+            active = false;
+        }
+        
+    });
+    $( window ).resize(function() {
+        var windowWidth = $(window).width();
+        if (windowWidth >= 580*.98) {
+            $(".barActive").hide(); 
+        }
+    });
+});
